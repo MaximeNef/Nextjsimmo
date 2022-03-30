769e8fd5fbe3a07a1a1f5fc924d8c6f3028a93b9
ronsmans-kevin <ronsmans.kev@gmail.com>
2022-03-30 15:59:19 +0200

    create header + pages maisons app et contact

---
+++ my-project/src/pages/appartements/index.jsx
@@ -0,0 +1,10 @@
+import Header from "../../components/all/header";
+
+const Appartements = () => {
+    return (
+        <div className="bg-[#3B3C3E] h-screen">Welcome page Appartements
+            <Header/>
+        </div>
+    );
+};
+export default Appartements 

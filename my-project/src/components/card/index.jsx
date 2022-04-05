01f773d7f852aa16da97854a89210e993db8106f
Nef <maximenef1@gmail.com>
2022-04-05 09:45:32 +0200

    add price container corner left

--- my-project/src/components/card/index.jsx
+++ my-project/src/components/card/index.jsx
@@ -3,13 +3,20 @@ import Image from "next/image";
 import Infocard from "./infoCard";
 const Card = () => {
   return (
-    <Container className="bg-white text-black mx-6 my-4 min-h-[400px] rounded-xl shadow-2xl">
-      <Image
-        src="/assets/card/villa.png"
-        width={"100vw"}
-        height={300}
-        className="rounded-t-xl"
-      />
+    <Container className=' text-black mx-10 my-4 rounded-xl shadow-2xl bg-white '>
+      <Container className='my-4 mx-1'>
+        <Container className=' ml-4 mb-[-40px] z-10 bg-white rounded-[250px] mr-auto shadow-2xl px-3 py-1'>
+          {" "}
+          897.945€
+        </Container>
+        <Image
+          src='/assets/card/villa.png'
+          width={"100vw"}
+          height={300}
+          className='rounded-t-xl p-1'
+        />
+      </Container>
+
       <Infocard />
     </Container>
   );
--- my-project/src/components/card/infoCard/index.jsx
+++ my-project/src/components/card/infoCard/index.jsx
@@ -2,13 +2,13 @@ import Container from "../../shared/composers/container";
 import Flex from "../../shared/composers/flex";
 const Infocard = () => {
   return (
-    <Container className="mx-10 my-auto">
-      <Container className="shadow-xl mr-auto rounded-xl text-xl font-bold tracking-wide">
+    <Container className='mx-10   '>
+      <Container className='shadow-xl mr-auto rounded-xl text-xl font-bold tracking-wide'>
         BRAINE-L'ALLEUD
       </Container>
-      <Flex justify="between">
-        <Container className="shadow-xl mr-auto rounded-xl">price</Container>
-        <Container className="shadow-xl ml-auto rounded-xl">peb</Container>
+      <Flex justify='between'>
+        <Container className='shadow-xl mr-auto rounded-xl'>price</Container>
+        <Container className='shadow-xl ml-auto rounded-xl'>peb</Container>
       </Flex>
     </Container>
   );

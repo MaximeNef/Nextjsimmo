72341f0fc68fa1333f1b81bbfe59f1bb21dd3fa5
Nef <maximenef1@gmail.com>
2022-04-12 10:14:07 +0200

    create card not finish

---
+++ my-project/src/components/card/favoriteCard/index.jsx
@@ -0,0 +1,12 @@
+import Container from "../../shared/composers/container";
+
+const FavoriteCard = () => {
+  return (
+    <Container className='w-16 h-16 bg-gray-600 rounded-full shadow-black shadow-2xl'>
+      <Container className='w-14 h-14 bg-gray-500 rounded-full m-auto items-center justify-center'>
+        {" <3"}
+      </Container>
+    </Container>
+  );
+};
+export default FavoriteCard;
--- my-project/src/components/card/index.jsx
+++ my-project/src/components/card/index.jsx
@@ -1,23 +1,46 @@
 import Container from "../shared/composers/container";
+import Flex from "../shared/composers/Flex";
 import Image from "next/image";
-import Infocard from "./infoCard";
+import FavoriteCard from "./FavoriteCard";
 const Card = () => {
   return (
-    <Container className=' text-black mx-10 my-4 rounded-xl shadow-2xl bg-white '>
-      <Container className='my-4 mx-1'>
-        <Container className=' ml-4 mb-[-40px] z-10 bg-white rounded-[250px] mr-auto shadow-2xl px-3 py-1'>
-          {" "}
-          897.945€
-        </Container>
-        <Image
-          src='/assets/card/villa.png'
-          width={"100vw"}
-          height={300}
-          className='rounded-t-xl p-1'
-        />
-      </Container>
+    <Container className=' mx-10  my-10 p-8 h-[600px]  rounded-[30px] shadow-2xl relative'>
+      <Image
+        src='/assets/card/villa.png'
+        quality={100}
+        layout={"fill"}
+        className=' mx-10 bg-black h-[600px] my-10  rounded-[30px] '
+      />
 
-      <Infocard />
+      <Flex type='col' justify='center'>
+        <Flex
+          className='relative ml-auto items-center shadow-black shadow-2xl
+            
+          bg-gray-600 rounded-full px-3 py-2 text-center max-h-[50px] w-[118px]'
+        >
+          <Flex
+            type='col'
+            align='center'
+            justify='center'
+            className='m-auto pt-1'
+          >
+            <Container className=' rounded-full h-3 w-3 bg-red-700 m-auto '></Container>{" "}
+            <Container className='h-[10px] w-[1px] bg-white mx-auto mb-2'></Container>
+          </Flex>
+          <Flex className='font-semibold text-lg pr-4'> Maps</Flex>
+        </Flex>{" "}
+        <Flex className='relative '>
+          <Flex className='min-w-[280px] items-end text-3xl font-extrabold leading-10'>
+            {"  Black Modern House"}
+          </Flex>
+          <Flex className='items-end justify-end'>
+            <FavoriteCard />
+          </Flex>
+        </Flex>
+        <Container className='relative mt-5 text-slate-200 text-base'>
+          {"  Broadway street, New York"}
+        </Container>
+      </Flex>
     </Container>
   );
 };

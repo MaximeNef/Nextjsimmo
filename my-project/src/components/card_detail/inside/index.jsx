dec102c9a3a9ec0380bd028284ea092063474b26
ronsmans-kevin <ronsmans.kev@gmail.com>
2022-04-15 12:17:10 +0200

    add details-inside 2

--- my-project/src/components/card_detail/inside/index.jsx
+++ my-project/src/components/card_detail/inside/index.jsx
@@ -7,35 +7,35 @@ const Inside = () => {
       <Flex className="text-base font-bold leading-6">Détails intérieurs</Flex>
       <Flex justify="between" className="mt-4">
         <Flex className="text-sm">
-          <Image src="/assets/details/terrace.svg" height={16} width={16} />
+          <Image src="/assets/details/bed.svg" height={16} width={16} />
           <Container className="ml-3">Chambres</Container>
         </Flex>
         <Container className="text-sm">4</Container>
       </Flex>
       <Flex justify="between" className="mt-4">
         <Flex className="text-sm">
-          <Image src="/assets/details/garage.svg" height={16} width={16} />
+          <Image src="/assets/details/shower.svg" height={16} width={16} />
           <Container className="ml-3">Salle de bain</Container>
         </Flex>
         <Container className="text-sm">1</Container>
       </Flex>
       <Flex justify="between" className="mt-4">
         <Flex className="text-sm">
-          <Image src="/assets/details/parking.svg" height={16} width={16} />
+          <Image src="/assets/details/wc.svg" height={16} width={16} />
           <Container className="ml-3">WC</Container>
         </Flex>
         <Container className="text-sm">1</Container>
       </Flex>
       <Flex justify="between" className="mt-4">
         <Flex className="text-sm">
-          <Image src="/assets/details/parking.svg" height={16} width={16} />
+          <Image src="/assets/details/desk.svg" height={16} width={16} />
           <Container className="ml-3">Bureau</Container>
         </Flex>
         <Container className="text-sm">4</Container>
       </Flex>
       <Flex justify="between" className="mt-4">
         <Flex className="text-sm">
-          <Image src="/assets/details/window.svg" height={16} width={16} />
+          <Image src="/assets/details/storage.svg" height={16} width={16} />
           <Container className="ml-3">Cave</Container>
         </Flex>
         <Container>

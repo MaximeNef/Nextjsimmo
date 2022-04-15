25669ea728c5e3db8402c667ea2f856b8f48d081
Nef <maximenef1@gmail.com>
2022-04-15 12:44:07 +0200

    change color icons card

--- my-project/src/components/card_detail/footerDetails/BtnContact/index.jsx
+++ my-project/src/components/card_detail/footerDetails/BtnContact/index.jsx
@@ -1,10 +1,13 @@
+import Link from "next/link";
 import Container from "../../../shared/composers/container";
 
 const BtnContact = () => {
   return (
-    <Container className=' text-lg font-semibold bg-black text-white p-4 rounded-2xl'>
-      reserve now
-    </Container>
+    <Link href='/contact'>
+      <Container className=' text-lg font-semibold bg-black text-white p-4 rounded-2xl'>
+        reserve now
+      </Container>
+    </Link>
   );
 };
 export default BtnContact;
--- my-project/src/components/card_detail/image/backArrow/index.jsx
+++ my-project/src/components/card_detail/image/backArrow/index.jsx
@@ -1,20 +1,26 @@
 import Image from "next/image";
 import Container from "../../../shared/composers/container";
 import Link from "next/link";
+import { useRouter } from "next/router";
 const BackArrow = ({ onClick }) => {
+  const router = useRouter();
+  function handleClick(href) {
+    router.push("/");
+  }
   return (
-    <Link href='/'>
-      <Container className='w-16 h-16 rounded-3xl bg-gray-300 shadow-black shadow-2xl bg-clip-padding backdrop-filter backdrop-blur-xl'>
-        <Container className='w-14 h-14 bg-white rounded-[20px] m-auto items-center justify-center shadow-white shadow-2xl bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-90'>
-          <Image
-            src='/assets/details/ArrowBack.svg'
-            alt='Picture of the author'
-            width={30}
-            height={30}
-          />
-        </Container>
+    <Container
+      onClick={handleClick}
+      className='w-16 h-16 rounded-3xl bg-gray-300 shadow-black shadow-2xl bg-clip-padding backdrop-filter backdrop-blur-xl'
+    >
+      <Container className='w-14 h-14 bg-white rounded-[20px] m-auto items-center justify-center shadow-white shadow-2xl bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-90'>
+        <Image
+          src='/assets/details/ArrowBack.svg'
+          alt='Picture of the author'
+          width={30}
+          height={30}
+        />
       </Container>
-    </Link>
+    </Container>
   );
 };
 export default BackArrow;
